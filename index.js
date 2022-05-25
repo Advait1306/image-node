df83e35b9a5759d135708ffe8bdd8269e51669bc
advait <advaitbansode4@gmail.com>
2022-05-25 12:58:34 +0530

    FEATURE: end

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const sharp = require('sharp');
 const download = require('image-downloader')
 
 app.get('/api', (req, res) => {
-    res.send("Hello")
-
     console.log(req.query)
     const url = req.query.url
+
     const options = {
         url: url,
-        dest: '../../images/',               // will be saved to /path/to/dest/image.jpg
+        dest: '../../images/',               
       };
 
 
     download.image(options)
       .then(({ filename }) => {
-        console.log('Saved to', filename); // saved to /path/to/dest/image.jpg
+        console.log('Saved to', filename);
+
+        const height = parseInt(req.query.height)
+        const width = parseInt(req.query.width)
+
+        if(height || width) {
+            sharp(filename)
+                .resize({width: width, height: height})
+                .png()
+                .toFile(filename+'resized.png')
+        }
+
+        // if(height && !width){
+        //     sharp(filename)
+        //         .resize({height: height})
+        //         .png()
+        //         .toFile(filename+'resized.png')
+        // }
+
+        // if(width && !height){
+        //     sharp(filename)
+        //         .resize({width: width})
+        //         .png()
+        //         .toFile(filename+'resized.png')
+        // }
+        
+
       })
       .catch((err) => console.error(err));
+
+    
 })
 
 app.listen(port, ()=>{
